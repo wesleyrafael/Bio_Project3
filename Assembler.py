8740fd91b8dfab26921a98405d1fa6cfc397eed2
Wesley Rafael dos Santos Sousa <rafael.0404@hotmail.com>
2019-05-27 14:47:06 -0300

    Parsing do arquivo texto corrigido e finalizado

--- Assembler.py
+++ Assembler.py
@@ -6,3 +6,11 @@ root.withdraw()
 
 file_path = filedialog.askopenfilename(title = "Selecionar arquivo kdMer",filetypes = (("Arquivos de Texto","*.txt"),("Todos os arquivos","*.*")))
 
+content = ''
+with open(file_path,"r") as file:
+    content = file.readline().strip()
+
+
+kdmers = content.replace(' ','').replace('\'','').replace('[','').replace(']','').split(',')
+for kdmer in kdmers:
+    print(kdmer)
